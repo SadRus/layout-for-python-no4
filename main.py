6687af5f69d08aed60dd8c667e8aff74ca6cac4d
SadRus <rystixx@yandex.ru>
2023-02-11 18:54:53 +0300

    Download book covers

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from requests import HTTPError
+from urllib.parse import urljoin
 
 
 def check_for_redirect(response):
@@ -19,6 +20,17 @@ def check_redirect_for_download(response):
 def download_txt(url, filename, folder='books/'):
     response = requests.get(url)
     response.raise_for_status()
+    check_redirect_for_download(response)
+
+    filename = sanitize_filename(filename)
+    full_path = os.path.join(folder, filename) 
+    with open(full_path, 'wb') as file:
+        file.write(response.content)
+
+
+def download_images(url, filename, folder='images/'):
+    response = requests.get(url)
+    response.raise_for_status()
 
     filename = sanitize_filename(filename)
     full_path = os.path.join(folder, filename) 
@@ -28,6 +40,7 @@ def download_txt(url, filename, folder='books/'):
 
 def main():
     os.makedirs('./books', exist_ok=True)
+    os.makedirs('./images', exist_ok=True)
     sep = ' \xa0 :: \xa0 '
 
     for book_id in range(1, 11):
@@ -41,17 +54,19 @@ def main():
         soup = BeautifulSoup(response.text, 'lxml')
         book_title_text = soup.find('h1').text
         book_title = book_title_text.split(sep)[0].strip()
-        filename = f'{book_id}. {book_title}.txt'
+        book_filename = f'{book_id}. {book_title}.txt'
 
         url_book_content = f'http://tululu.org/txt.php?id={book_id}'
         try:
-            response = requests.get(url_book_content)
-            response.raise_for_status()
-            check_redirect_for_download(response)
+            download_txt(url_book_content, book_filename)
         except HTTPError:
             continue
     
-        download_txt(url_book_content, filename=filename)
+        book_image_src = soup.find(class_='bookimage').find('img')['src']
+        book_image_url = urljoin(url_book, book_image_src)
+        image_filename = book_image_src.split('/')[-1]
+        download_images(book_image_url, image_filename)
+
 
 if __name__ == '__main__':
     main()
