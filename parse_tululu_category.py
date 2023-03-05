50b51c68cb2b8f0bd12996075a530168ce244d9b
SadRus <rystixx@yandex.ru>
2023-03-05 21:46:59 +0300

    Sanitize_filename title before adding to book_path in parse_book_page()

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -46,7 +46,7 @@ def parse_book_page(response):
     title, author = title_tag.text.split(' \xa0 :: \xa0 ')
     title, author = title.strip(), author.strip()
     img_src = soup.select_one('.bookimage img')['src']
-    book_path = urljoin('books/', f'{book_id}. {title}.txt')
+    book_path = urljoin('books/', f'{book_id}. {sanitize_filename(title)}.txt')
     book_comments = soup.select('.texts .black')
     book_comments_text = [comment.text for comment in book_comments]
     genres = soup.select('.ow_px_td span.d_book a')
